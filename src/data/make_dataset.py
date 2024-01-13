dce3ebf31098da672e60f40789b1129bceeada85
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-01-13 17:39:51 -0500

    Update url variable

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,5 @@
 import asyncio
-from dataclasses import dataclass, field, fields
+from dataclasses import dataclass, field
 from time import perf_counter
 
 import httpx
@@ -16,20 +16,13 @@ class Magazine:
 
 async def main():
     async with httpx.AsyncClient(http2=True) as client:
-        dtype_dict = {}
-        field_names = [field.name for field in fields(Magazine)]
-
-        for field in field_names:
-            dtype_dict.update(gather_covers.parse_dtype(Magazine, field))
-
         magazine_cover_links = []
         magazine_desc = []
 
-        page_range = range(
-            13, 0, -1
-        )  # 13 pages of results - set manually since you get 200 status even on out of bound pages.
+        page_range = gather_covers.page_range
 
         cover_url = "https://www.eatyourbooks.com/magazines/cooks-illustrated/"
+        cover_url_stem = "https://www.eatyourbooks.com/"
 
         start_time = perf_counter()
 
@@ -38,9 +31,11 @@ async def main():
         )
 
         for page in top_level_cover_html:
-            magazine_cover_links = gather_covers.parse_cover_index_page(page)
+            magazine_cover_links = gather_covers.parse_cover_index_page(
+                cover_url_stem, page
+            )
             for link in magazine_cover_links:
-                print(link)
+                print(f"Retrieving {link}")
                 individual_cover_html = await gather_covers.fetch_cover_html(
                     client, link
                 )
@@ -55,7 +50,8 @@ async def main():
 
         print(f"Total time to scrape: {(end_time - start_time):.2f} seconds")
 
-        df = pl.from_records(magazine_desc, schema=dtype_dict)
+        dtype_dict = gather_covers.gather_dtype(Magazine)  # type: ignore
+        df = pl.from_records(magazine_desc, schema=dtype_dict)  # type: ignore
         print(df)
         df.write_parquet("./data/raw/magazine_dump.parquet")
         df.write_csv(

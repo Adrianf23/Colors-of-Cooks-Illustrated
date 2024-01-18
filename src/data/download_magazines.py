51c8574247937c27612abed6c5f9da654ec6fb6d
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-01-18 03:23:21 -0500

    Script for scraping and downloading images

--- src/data/download_magazines.py
+++ src/data/download_magazines.py
@@ -1,10 +1,9 @@
+import httpx
+import polars as pl
 from dataclasses import dataclass, field, fields
 from pathlib import Path
-import re
-import httpx
 from selectolax.parser import HTMLParser
-from playwright.async_api import async_playwright
-import polars as pl
+from typing import Any, Generator
 
 
 @dataclass
@@ -13,24 +12,16 @@ class Magazine:
     image_link: str | None = field(metadata={"dtype": pl.Utf8})
 
 
-def clean_link(
-    text: str,
-) -> str:
+def parse_item(html_page: HTMLParser) -> Generator[Magazine, Any, None]:
     """
-    clean_link Extract text from a url, removing the text between two parantheses
+    parse_item Take an HTMLParser object, return image and alt text
 
     Args:
-        text (str): String to replace
+        `html_page (HTMLParser)`: HTMLParser object from an HTTP request
 
-    Returns:
-        str: Cleaned string
+    Yields:
+        `Generator[Magazine, Any, None]`: Generator object of image and alt text
     """
-    regex = r"\bupload\b(.+?)\/"  # this removes the text that is cropping the magazine covers in the browser
-    replacement = "upload/"
-    return re.sub(regex, replacement, text)
-
-
-def parse_item(html_page: HTMLParser):
     data = html_page.css("picture.StandardCardImage_cardImage__pH9Yg")
     for item in data:
         yield (
@@ -46,23 +37,23 @@ def parse_dtype(self, attribute_name: str) -> dict[str, str]:
     parse_dtype Parse the dtype metadata from a dataclass
 
     Args:
-        attribute_name (str): Attribute in metadata of dataclass
+        `attribute_name (str)`: Attribute in the metadata of a dataclass
 
     Returns:
-        dict: Dictionary of metadata attribute and accompanying dtype
+        `dict[str, str]`: Dictionary of the metadata attribute and accompanying dtype from a dataclass
     """
     return {attribute_name: self.__dataclass_fields__[attribute_name].metadata["dtype"]}
 
 
 def gather_dtype(dataclass_obj: Magazine) -> dict[str, str]:
     """
-    gather_dtype Create a dictionary of dtypes from the dataclass metadata property
+    gather_dtype Return a dictionary of fields and associated dtypes from a dataclass
 
     Args:
-        dataclass_obj (Magazine): Dataclass object of type Magazine
+        `dataclass_obj (Magazine)`: Magazine dataclass
 
     Returns:
-        dict[str, str]: Dictionary of {field: datatype} for each field in the Magazine dataclass
+        `dict[str, str]`: Dictionary of {field: datatype} for each field in the Magazine dataclass
     """
     dtype_dict = {}
     field_names = [field.name for field in fields(dataclass_obj)]
@@ -71,20 +62,83 @@ def gather_dtype(dataclass_obj: Magazine) -> dict[str, str]:
     return dtype_dict
 
 
+# year code source: https://stackoverflow.com/a/77836528/8646265
+
+
+# I chained a select statement after the with_column statement since I needed the year and months to create the filename. Source: https://stackoverflow.com/a/75601785/8646265
+def clean_df_name(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    clean_df_name Return a polars dataframe with some transformations applied
+
+    Args:
+        `df (pl.DataFrame)`: Polars DataFrame
+
+    Returns:
+        `pl.DataFrame`: Polars DataFrame
+    """
+    return df.with_columns(
+        year=("1 " + pl.col("name").str.replace("/.* ", ""))
+        .str.to_datetime("%d %B %Y")
+        .dt.year(),
+        start_month_num=("1 " + pl.col("name").str.replace("/.* ", ""))
+        .str.to_datetime("%d %B %Y")
+        .dt.month(),
+        end_month_num=pl.col("name")
+        .str.replace(".*/", "1 ")
+        .str.to_datetime("%d %B %Y")
+        .dt.month(),
+        start_month_str=pl.col("name").str.extract(r"(\w+)\/"),
+        end_month_str=pl.col("name").str.extract(r"\/(\w+)"),
+        cleaned_link=pl.col("image_link").str.replace(r"\bupload\b(.+?)\/", "upload/"),
+        # Some of the magazines are blank and all have the same link
+        is_blank_cover=pl.when(
+            pl.col("image_link").str.contains(
+                r"https://res.cloudinary.com/hksqkdlah/image/upload/Recipe_Default_zbu7tq"
+            )
+        )
+        .then(pl.lit(1, dtype=pl.Int8))
+        .otherwise(pl.lit(0, dtype=pl.Int8)),
+    ).select(
+        pl.col("*").exclude("image_link"),
+        filename=pl.concat_str(
+            [
+                pl.col("year"),
+                pl.col("start_month_num"),
+                pl.col("end_month_num"),
+            ],
+            separator="_",
+        ),
+    )
+
+
 def create_img_folder() -> Path:
+    """
+    create_img_folder Return a pathlib object from user input
+
+    Returns:
+        `Path`: Pathlib object
+    """
     folder = input(str("Enter a folder to save your images: "))
     filepath = Path.cwd() / "data" / "raw" / folder
-    Path.mkdir(filepath, exist_ok=True)
+    Path.mkdir(filepath, parents=True, exist_ok=True)
     print(f"Images will be saved here: {filepath}")
     return filepath
 
 
 async def download_images(df: pl.DataFrame, client: httpx.AsyncClient) -> None:
+    """
+    download_images Return a list of images to the specified folder in `create_img_folder`
+
+    Args:
+        `df (pl.DataFrame)`: Polars DataFrame
+        `client (httpx.AsyncClient)`: HTTPx AsyncClient
+    """
     filepath = create_img_folder()
 
-    for name, image_link in df.rows():
-        filename = (filepath / name).with_suffix(".jpg")
-        async with client.stream("GET", image_link) as response:
+    # Inefficient implentation. TODO: Find columnar implementation
+    for row in df.iter_rows(named=True):
+        filename = (filepath / row["filename"]).with_suffix(".jpg")
+        async with client.stream("GET", row["cleaned_link"]) as response:
             print("Downloading the images...")
             with open(filename, "wb") as f:
                 print("Writing data to file")

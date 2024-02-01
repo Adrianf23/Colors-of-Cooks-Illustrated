fb25dc300ec093d7ced51e79bf8e5e723e5e9bf7
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-02-01 03:12:54 -0500

    Added kmeans analysis script

--- src/data/kmeans_image_download.py
+++ src/data/kmeans_image_download.py
@@ -1,31 +1,33 @@
+from kmeans import kmeans_img
+import polars as pl
+from pathlib import Path
 from time import perf_counter
-from 
-
-image_filepath = Path.cwd() / "data" / "raw" / "cooks-illustrated"
-
-files = [str(path) for path in image_filepath.rglob("*") if path.is_file()]
 
 
-start = perf_counter()
 
-parquet_filepath = Path.cwd() / "data" / "interim"
+def main():
+    image_filepath = Path.cwd() / "data" / "raw" / "cooks-illustrated"
+    files = [str(path) for path in image_filepath.rglob("*") if path.is_file()]
 
-for i, v in enumerate(files):
-    if Path(f"{parquet_filepath / v.split("\\")[-1].strip(".jpg")}_cover.parquet").exists() == True:
-        continue
-    else:
-        print(f"New file: {v}")
-        print("Exporting to parquet")
-        print("....................")
-        pl.from_records(
-            kmeans_img(filepath=files[i], n_clusters=2), schema=["filepath", "og_image", "segmented_image", "image_labels"]
-        ).write_parquet(
-            file=f"{parquet_filepath/v.split("\\")[-1].strip(".jpg")}_cover.parquet", compression="zstd", compression_level=20, use_pyarrow=True
-        )
-        print("....................")
+    start = perf_counter()
+    parquet_filepath = Path.cwd() / "data" / "interim"
 
+    for i, v in enumerate(files):
+        if Path(f"{parquet_filepath / v.split("\\")[-1].strip(".jpg")}_cover.parquet").exists() == True:
+            continue
+        else:
+            print(f"New file: {v}")
+            print("Exporting to parquet")
+            print("....................")
+            pl.from_records(
+                kmeans_img(filepath=files[i], n_clusters=2), schema=["filepath", "og_image", "segmented_image", "image_labels"]
+            ).write_parquet(
+                file=f"{parquet_filepath/v.split("\\")[-1].strip(".jpg")}_cover.parquet", compression="zstd", compression_level=20, use_pyarrow=True
+            )
+            print("....................")
+    end = perf_counter()
+    print("Finished exporting all files")
+    print(f"End of script. Took {end-start} seconds.")
 
-end = perf_counter()
-
-print("Finished exporting all files")
-print(f"End of script. Took {end-start} seconds.")
+if __name__ == "__main__":
+    main()

eaa35f100329a628d0c941cd417854ce5434a855
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-02-01 02:41:39 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A color analysis of the covers of Cook\'s Illustrated from 1992-2024 using k-means clustering.',
-    author='Adrian Fletcher',
-    license='MIT',
+    version="0.1.0",
+    description="A color analysis of the covers of Cook's Illustrated from 1992-2024 using k-means clustering.",
+    author="Adrian Fletcher",
+    license="MIT",
 )

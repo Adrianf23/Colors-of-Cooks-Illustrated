1a4e1bd4183ad2788f5791067e4d0cd9ad4478c9
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-02-13 12:34:18 -0500

    Updated type annotations inside the function body

--- src/data/kmeans.py
+++ src/data/kmeans.py
@@ -29,12 +29,14 @@ def kmeans_img(*, filepath: str | Path, n_clusters: int) -> list[list[Any]]:
     with Image.open(filepath) as img:
         # Convert any RGBA -> RGB
         if np.asarray(img).shape[2] == 4:
-            X = np.asarray(img.convert(mode="RGB")).reshape(-1, 3)
+            X: np.ndarray = np.asarray(img.convert(mode="RGB")).reshape(-1, 3)
         else:
             X = np.asarray(img).reshape(-1, 3)
-        kmeans = KMeans(n_clusters=n_clusters)
+        kmeans: KMeans = KMeans(n_clusters=n_clusters)
         kmeans.fit(X)
-        segmented_image = kmeans.cluster_centers_[kmeans.labels_].reshape(-1, 3)
+        segmented_image: np.ndarray = kmeans.cluster_centers_[kmeans.labels_].reshape(
+            -1, 3
+        )
 
     return [
         [filepath],

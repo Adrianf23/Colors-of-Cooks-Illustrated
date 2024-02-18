03449752eb68b1cf171be2832e560ff0412abf96
Adrian Fletcher <adrianfletcher3@gmail.com>
2024-02-18 17:19:12 -0500

    Refactored functions to perform KMeans and color squares in one function. Note: switched from using Polars.

--- src/data/run_kmeans.py
+++ src/data/run_kmeans.py
@@ -1,33 +1,91 @@
+import matplotlib.pyplot as plt
 import polars as pl
-from kmeans import kmeans_img
-from pathlib import Path, PurePath
+from kmeans import get_color_labels, kmeans_img
+from pathlib import Path
+from PIL import Image, ImageDraw
 from time import perf_counter
 
 
 def main():
     image_filepath: Path = Path.cwd() / "data" / "raw" / "cooks-illustrated"
-    files: list[str] = [
-        str(path) for path in image_filepath.rglob("*") if path.is_file()
+
+    # tuple of (filepath and file stem) - filepath is for kmeans and file stem is for filtering on the below magazine dataframe
+    files: list[tuple[str, str]] = [
+        (str(path), path.stem) for path in image_filepath.rglob("*") if path.is_file()
     ]
 
-    # TODO: Load in the magazine_cover.csv and filter for the nonblank images
-    # TODO: Filter the `files` list to only return the images we will be using
-
-    kmean_folder: str = input(
-        str("Name the folder where the KMeans data will be stored: ")
+    magazine_filepath: Path = Path.cwd() / "data" / "raw" / "magazine_covers.csv"
+    magazine_covers: pl.DataFrame = (
+        pl.read_csv(magazine_filepath)
+        .filter(pl.col.is_blank_cover != 1)
+        .select(pl.exclude("is_blank_cover"))
     )
-    csv_filepath: Path = Path.cwd() / "data" / "interim" / kmean_folder
-    Path.mkdir(csv_filepath, exist_ok=True)
-    print(f"KMeans data will be saved here: {csv_filepath}")
+
+    filtered_magazines: list[str] = magazine_covers.get_column("filename").to_list()
+    filtered_files: list[str] = [
+        file[0] for file in files if file[1] in filtered_magazines
+    ]
+
+    kmeans_folder: str = input(
+        str("Name the folder where the KMeans color squares will be stored: ")
+    )
+    color_square_filepath: Path = Path.cwd() / "data" / "processed" / kmeans_folder
+    Path.mkdir(color_square_filepath, exist_ok=True)
+    print(f"Segmented images will be saved here: {color_square_filepath}")
 
     start: float = perf_counter()
+    for file in filtered_files:
+        if (
+            Path(f"{color_square_filepath}/{Path(file).stem}-square.webp").exists()
+            == True
+        ):
+            continue
+        else:
+            color_palette, color_labels = kmeans_img(filepath=file, n_clusters=10)
 
-    for file in files:
-        file_header: str = PurePath(file).stem
-        # TODO: Run kmeans function here and just save the segmented image
-        segmented_image = kmeans_img(filepath=file, n_clusters=10)
+            color_square_list: list = get_color_labels(color_labels)
 
-        # TODO: Run logic for saving the top 10 images
+            image: Image.Image = Image.new("RGB", (200, 200), "white")
+            draw = ImageDraw.Draw(image)
+
+            # Define the coordinates for the big square
+            big_square_coords: tuple[int, int, int, int] = (0, 0, 200, 200)
+
+            # Define the coordinates for the three inner squares
+            inner_square1_coords: tuple[int, int, int, int] = (25, 25, 100, 175)
+            inner_square2_coords: tuple[int, int, int, int] = (100, 25, 175, 100)
+            inner_square3_coords: tuple[int, int, int, int] = (100, 100, 175, 175)
+
+            # Draw the big square
+            draw.rectangle(
+                big_square_coords,
+                fill=tuple(color_palette[color_square_list[0]].astype(int)),
+            )
+
+            # Draw the three inner squares
+            draw.rectangle(
+                inner_square1_coords,
+                fill=tuple(color_palette[color_square_list[1]].astype(int)),
+            )
+            draw.rectangle(
+                inner_square2_coords,
+                fill=tuple(color_palette[color_square_list[2]].astype(int)),
+            )
+            draw.rectangle(
+                inner_square3_coords,
+                fill=tuple(color_palette[color_square_list[3]].astype(int)),
+            )
+
+            # Display the image
+            plt.imshow(image)
+            plt.axis("off")
+
+            plt.savefig(
+                f"{color_square_filepath}/{Path(file).stem}-square.webp",
+                bbox_inches="tight",
+                pad_inches=0,
+                dpi=300,
+            )
     end: float = perf_counter()
     print("Finished exporting all files")
     print(f"End of script. Took {end-start} seconds.")
